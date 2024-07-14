01ec6654d46ae9a21f93c643fc9832b041fbe411
maryreagan <maryreagan630@yahoo.com>
2024-07-14 18:49:37 -0500

    minimum buildout of server

--- server/controllers/board.js
+++ server/controllers/board.js
@@ -24,9 +24,37 @@ router.get("/:id", async (req, res) => {
 })
 
 // Create a board
+router.post("/", async (req, res) => {
+    const newBoard = new Board(req.body);
+    try{
+        const saveBoard = await newBoard.save();
+        res.status(200).json(saveBoard);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 // Update a board
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const updateBoard = await Board.findByIdAndUpdate(id, {$set: req.body});
+        res.status(200).json(updateBoard);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+)
 
 // Delete a board
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const deleteBoard = await Board.findByIdAndDelete(id);
+        res.status(200).json(deleteBoard);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
--- server/controllers/player.js
+++ server/controllers/player.js
@@ -16,4 +16,71 @@ router.get("/", async (req, res) => {
     }
 })
 
+// Get a player by id
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const findPlayer = await Player.find({_id: id});
+        res.status(200).json(findPlayer);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// Register a player
+router.post("/register", async (req, res) => {
+    const newPlayer = new Player(req.body);
+    try{
+        const hashedPassword = bcrypt.hashSync(newPlayer.password, SALT);
+        newPlayer.password = hashedPassword;
+        const savePlayer = await newPlayer.save();
+        res.status(200).json(savePlayer);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// Login a player
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        const findPlayer = await Player.findOne({email: email});
+        if(!findPlayer){
+            res.status(400).json("Invalid email or password");
+        }
+        const validPassword = await bcrypt.compare(password, findPlayer.password);
+        if(!validPassword){
+            res.status(400).json("Invalid email or password");
+        }
+        const token = jwt.sign({_id: findPlayer._id}, JWT_SECRET_KEY);
+        res.status(200).json(token);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+)
+
+// Update a player
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const updatePlayer = await Player.findByIdAndUpdate(id, {$set: req.body});
+        res.status(200).json(updatePlayer);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+)
+
+// Delete a player
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    try{
+        const deletePlayer = await Player.findByIdAndDelete(id);
+        res.status(200).json(deletePlayer);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
